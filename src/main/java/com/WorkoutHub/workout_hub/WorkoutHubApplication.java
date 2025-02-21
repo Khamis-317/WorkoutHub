586c90bb292317cab804a9b418c1607021c6acca
khamis <mohamedkhamis20045@gmail.com>
2025-02-21 20:48:07 +0200

    intial

---
+++ src/main/java/com/WorkoutHub/workout_hub/WorkoutHubApplication.java
@@ -0,0 +1,13 @@
+package com.WorkoutHub.workout_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutHubApplication.class, args);
+	}
+
+}
