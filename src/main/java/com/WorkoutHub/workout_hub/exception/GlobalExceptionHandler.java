5189e1ece346906b540ef012444e39f469f1feb9
khamis <mohamedkhamis20045@gmail.com>
2025-04-16 18:17:16 +0200

    Fix Controller Advise Annotation.

--- src/main/java/com/WorkoutHub/workout_hub/exception/GlobalExceptionHandler.java
+++ src/main/java/com/WorkoutHub/workout_hub/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.WorkoutHub.workout_hub.exception;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-@RestController
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 }
