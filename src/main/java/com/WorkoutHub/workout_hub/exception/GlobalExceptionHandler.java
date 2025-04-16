77f3905594acc4ba904c5e79a71d31024fbc2baa
khamis <mohamedkhamis20045@gmail.com>
2025-04-16 18:14:12 +0200

    Add Global Exception Handler, Generic Response Class for REST APIs, and Custom Exception Directories.

---
+++ src/main/java/com/WorkoutHub/workout_hub/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.WorkoutHub.workout_hub.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+}
