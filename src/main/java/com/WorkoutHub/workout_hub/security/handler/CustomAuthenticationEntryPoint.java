0b035b3e254334c30cc9e3fc1fabdb69feb57745
Mohamed Khamis <mohamedkhamis20045@gmail.com>
2025-12-24 18:21:08 +0200

    feat: implement custom authentication entry point for handling unauthorized access

---
+++ src/main/java/com/WorkoutHub/workout_hub/security/handler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.WorkoutHub.workout_hub.security.handler;
+
+import com.WorkoutHub.workout_hub.response.GenericResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        response.setContentType("application/json");
+
+        var jsonResponse = GenericResponse.error(authException.getMessage());
+
+        response.getWriter().write(objectMapper.writeValueAsString(jsonResponse));
+
+    }
+}
